e19a773657232aec6758415469615be3c75f7c34
raulvera59 <Raul@poppabear.home.gateway>
2015-08-01 10:05:01 +1000

    Simplified histogram calculation
    The gradient image is a single contiguous slice of pixels that we
    allocated, so to visit each pixel a simple range expression suffices.
    Removed all the fancy index calculations. No appreciable difference in
    speed, but the code is a lot simpler.

--- shist/shist.go
+++ shist/shist.go
@@ -11,46 +11,40 @@ import (
 )
 
 type Sipphist struct {
-	Grad *Gradimage
-	Bin [] uint32
-	Max uint32
-	K int
+	grad *Gradimage
+	bin [] uint32
+	max uint32
+	k int
 }
 
 func Hist(grad *Gradimage, k int) (hist *Sipphist) {
 	// create the 2D histogram bins as 2K+1 on a side, so always odd
 	hist = new(Sipphist)
-	hist.Grad = grad
-	hist.K = k
+	hist.grad = grad
+	hist.k = k
 	stride := 2*k+1
 	histDataSize := stride*stride
-	hist.Bin = make([] uint32, histDataSize)
+	hist.bin = make([] uint32, histDataSize)
 	fmt.Println("histogram side:", stride, " data size: ", histDataSize)
 	
 	// Walk through the image, computing the bin address from the gradient 
 	// values. 
-    gradStride := grad.Rect.Dx()
     factor := float64(k) / grad.MaxMod
-    fmt.Println("MaxMod:", grad.MaxMod, " factor:", factor) 
-	for line := 0; line < grad.Rect.Dy(); line++ {
-		lineMin := line*gradStride
-		lineMax := lineMin+gradStride
-		for gradIndex := lineMin; gradIndex < lineMax; gradIndex++ {
-			u := math.Floor(factor*float64(real(grad.Pix[gradIndex])) + float64(k)) 
-			v := math.Floor(factor*float64(imag(grad.Pix[gradIndex])) + float64(k))
-			histIndex := int(v)*stride + int(u)
-			if histIndex <0 || histIndex >= histDataSize {
-				fmt.Println("YOW! index out of range at line ", line, ":", histIndex,
-							" u:", u, " v:", v, "grad.Pix[gradIndex]:", grad.Pix[gradIndex])
-				return
-			}
-			hist.Bin[histIndex]++
-			if hist.Bin[histIndex] > hist.Max {
-				hist.Max = hist.Bin[histIndex]
-			}
-		}		
+    fmt.Println("MaxMod:", grad.MaxMod, " factor:", factor)
+	for _, pixel := range grad.Pix {
+		u := math.Floor(factor*float64(real(pixel)) + float64(k)) 
+		v := math.Floor(factor*float64(imag(pixel)) + float64(k))
+		histIndex := int(v)*stride + int(u)
+		hist.bin[histIndex]++
+		if hist.bin[histIndex] > hist.max {
+			hist.max = hist.bin[histIndex]
+		}
 	}
-	fmt.Println("Histogram complete. Maximum bin value:", hist.Max)
+	fmt.Println("Histogram complete. Maximum bin value:", hist.max)
+	return
+}
+
+func (hist *Sipphist) Entropy() (ent float64) {
 	return
 }
 
