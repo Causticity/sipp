e5fc88ee254c7c56661c031356eb76c24b7cf7f5
raulvera59 <Raul@poppabear.home.gateway>
2015-08-07 09:35:50 +1000

    Render and output all intermediate images.

--- sipp/main.go
+++ sipp/main.go
@@ -14,11 +14,11 @@ import (
 
 func main() {
 	var in = flag.String("in", "", "input image file; must be grayscale png")
-	var out = flag.String("out", "", "output image file")
+	var out = flag.String("out", "", "output image file prefix")
 	var k = flag.Int("K", 63, "Number of bins to scale the max radius to.\nThe histogram will be 2K+1 bins on a side")
 	flag.Parse()
 	fmt.Println("input file:<", *in, ">")
-	fmt.Println("output file:<", *out, ">")
+	fmt.Println("output file prefix:<", *out, ">")
 	fmt.Println("histogram edge size:", (*k*2+1))
 
 	var src, err = simage.Read(in)
@@ -30,15 +30,37 @@ func main() {
 	
 	grad := sgrad.Fdgrad(src)
 	fmt.Println("gradient image computed")
-	
-	hist := shist.Hist(grad, *k)
-	fmt.Println("Entropy of the gradient image:", hist.Entropy())
-	
-	rhist := hist.Render()
-		
-	err = rhist.Write(out)
+
+	re, im := grad.Render()
+	reName := *out + "_grad_real.png"
+	err = re.Write(&reName)
 	if err != nil {
-		fmt.Println("Error writing scatter-plot image:", err)
+		fmt.Println("Error writing real gradient image:", err)
 		os.Exit (1)
 	}
+	imName := *out + "_grad_imag.png"
+	err = im.Write(&imName)
+	if err != nil {
+		fmt.Println("Error writing imag gradient image:", err)
+		os.Exit (1)
+	}
+
+	hist := shist.Hist(grad, *k)
+	
+	rhist := hist.Render()
+	histName := *out + "_hist.png"	
+	err = rhist.Write(&histName)
+	if err != nil {
+		fmt.Println("Error writing histogram image:", err)
+		os.Exit (1)
+	}
+	histSup := hist.RenderSuppressed()
+	histSupName := *out + "_hist_sup.png"	
+	err = histSup.Write(&histSupName)
+	if err != nil {
+		fmt.Println("Error writing suppressed histogram image:", err)
+		os.Exit (1)
+	}
+
+	fmt.Println("Entropy of the gradient image:", hist.Entropy())
 }
